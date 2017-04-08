91747ae75c7e8d9726bf064ebc6b99210f69a564
Guy H Clark <guy.clark0@gmail.com>
2017-04-08 02:51:23 +0100

    Added new version of E2. Currently doesn't return correct coord value

--- src/Exersise2.java
+++ src/Exersise2.java
@@ -1,5 +1,6 @@
 /* TODO -
- * Clean up code by putting a lot of it into other functions */
+ * Clean up code by putting a lot of it into other functions 
+ * The returned Coord doesn't actually return the correct value. Find out why!*/
 
 import java.util.Arrays;
 public class Exersise2  {
@@ -7,10 +8,7 @@ public class Exersise2  {
 	/* Method to find mean and returns the position of the closest element */
 	public static int[] closestToMean (double[][] array){
 		double[][] difference = new double[3][4];
-		double min = Integer.MAX_VALUE;
-		int[] coord = new int[2];
-		int xCord = 0;
-		int yCord = 0;
+		int[] coords = new int[2];
 		printArray(array);
 		double sum =0;
 		int count=0;
@@ -24,14 +22,8 @@ public class Exersise2  {
 		double mean = sum/count;
 		System.out.println("array mean is: " + mean);
 		findDifference(array,difference,mean);
-		
-		for(int x=0; x<difference.length;x++){
-			for(int y=0;y<difference[0].length;y++){
-				if(difference[x][y] <min) {min = difference[x][y];coord[0]=x; coord[1] =y; xCord = x; yCord = y;}
-			}
-		}
-		System.out.println("The closest element is: " + array[xCord][yCord]);
-		return coord;
+		returnCoord(difference,coords);
+		return coords;
 	}
 
 	
@@ -50,7 +42,7 @@ public class Exersise2  {
 		numbers[2][1] = 2;
 		numbers[2][2] = 4;
 		numbers[2][3] =-9;
-		System.out.println("The co-ords for this element is" + Arrays.toString(closestToMean(numbers)));
+		System.out.println("The co-ords for this element are: " + Arrays.toString(closestToMean(numbers)));
 		
 	}
 	
@@ -73,6 +65,23 @@ public class Exersise2  {
 				
 			}
 		}
+	}
+	
+	static int[] returnCoord(double[][] difference, int[] coords){
+		double min = Integer.MAX_VALUE;
+		int[] coord = new int[2];
+		int xCord = 0;
+		int yCord = 0;
+		
+		for(int x=0; x<difference.length;x++){
+			for(int y=0;y<difference[0].length;y++){
+				if(difference[x][y] <min) {min = difference[x][y];coord[0]=x; coord[1] =y;}
+			}
+		}
+		
+		printArray(difference);
+		System.out.println(Arrays.toString(coord));
+		return coord;
 	}
 
 
